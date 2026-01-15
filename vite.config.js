16ec302a45ebfa52e4d6f4398d7aa582870089ea
Vidit Bhatnagar <bhatnagar007vidit@gmail.com>
2026-01-15 12:04:04 +0400

    First commit

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    assetsDir: 'assets',
   }
 })
